3a0758a6469597a336998f631a85ead56dc20446
Zak <zrodger7@gmail.com>
2022-11-14 21:25:43 +0000

    Added coroutine

--- CriminalIntent/app/src/main/java/com/example/android/criminalintent/CrimeListFragment.kt
+++ CriminalIntent/app/src/main/java/com/example/android/criminalintent/CrimeListFragment.kt
@@ -7,13 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.android.criminalintent.databinding.FragmentCrimeListBinding
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 private const val TAG = "CrimeListFragment"
 
 class CrimeListFragment: Fragment() {
-
 
     private var _binding: FragmentCrimeListBinding? = null
     private val binding
@@ -22,11 +26,6 @@ class CrimeListFragment: Fragment() {
         }
 
     private val crimeListViewModel: CrimeListViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,11 +36,19 @@ class CrimeListFragment: Fragment() {
 
         binding.crimeRecyclerView.layoutManager = LinearLayoutManager(context)
 
-        val crimes = crimeListViewModel.crimes
-        val adapter = CrimeListAdapter(crimes)
-        binding.crimeRecyclerView.adapter = adapter
-
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                val crimes = crimeListViewModel.loadCrimes()
+                binding.crimeRecyclerView.adapter =
+                    CrimeListAdapter(crimes)
+            }
+        }
     }
 
     override fun onDestroyView() {
--- CriminalIntent/app/src/main/java/com/example/android/criminalintent/CrimeListViewModel.kt
+++ CriminalIntent/app/src/main/java/com/example/android/criminalintent/CrimeListViewModel.kt
@@ -1,13 +1,30 @@
 package com.example.android.criminalintent
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.*
+
+private const val TAG = "CrimeListViewModel"
 
 class CrimeListViewModel: ViewModel() {
 
     val crimes = mutableListOf<Crime>()
 
     init {
+        Log.d(TAG, "init starting")
+        viewModelScope.launch{
+            Log.d(TAG, "coroutine launched")
+            crimes += loadCrimes()
+            Log.d(TAG, "Loading crimes finished")
+        }
+    }
+
+    suspend fun loadCrimes(): List<Crime> {
+        val result = mutableListOf<Crime>()
+        delay(5000)
         for (i in 0 until 100) {
             val crime = Crime(
                 id = UUID.randomUUID(),
@@ -16,8 +33,8 @@ class CrimeListViewModel: ViewModel() {
                 isSolved = i % 2 == 0,
                 requiresPolice = i % 10 != 0
             )
-
-            crimes += crime
+            result += crime
         }
+        return result
     }
 }
