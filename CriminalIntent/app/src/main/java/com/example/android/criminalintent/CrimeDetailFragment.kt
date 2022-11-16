0ebde81c714eb0fe17af9a8ab163120324268805
Zak <zrodger7@gmail.com>
2022-11-16 21:33:47 +0000

    Crime can no longer be left blank

--- CriminalIntent/app/src/main/java/com/example/android/criminalintent/CrimeDetailFragment.kt
+++ CriminalIntent/app/src/main/java/com/example/android/criminalintent/CrimeDetailFragment.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -30,6 +32,28 @@ class CrimeDetailFragment: Fragment() {
         get() = checkNotNull(_binding) {
             "Cannot access binding because it is null. Is the view visible?"
         }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (binding.crimeTitle.text.isEmpty()) {
+                    Toast.makeText(
+                        binding.root.context,
+                        "Crime cannot be left blank",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    // default onBackPressed() behavior
+                    isEnabled = false
+                    requireActivity().onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(callback)
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
